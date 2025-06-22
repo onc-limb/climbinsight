14f37e301909556b1d35d05966e3160c7162e738
onsato <nogiglee@gmail.com>
2025-06-22 11:58:53 +0900

    ページパス変更

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    images: {
-        domains: ["localhost"], // ← ← ← ここがポイント！
-    },
+  /* config options here */
+  images: {
+    domains: ["localhost"], // ← ← ← ここがポイント！
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/hold-extraction",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
