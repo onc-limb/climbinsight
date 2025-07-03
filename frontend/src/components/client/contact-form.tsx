54aee3bf7e25e60c747d74e20ee66a386c32e826
onsato <nogiglee@gmail.com>
2025-07-04 07:04:32 +0900

    fetch

--- frontend/src/components/client/contact-form.tsx
+++ frontend/src/components/client/contact-form.tsx
@@ -50,15 +50,13 @@ export default function ContactForm() {
     setSubmitSuccess(null);
 
     try {
-    //   const response = await fetch('/api/inquery', { // Goのバックエンドへのパスに合わせて適宜変更してください
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ category, email, message }),
-    //   });
-    console.log({category, email, message})
-    const response = {ok: true}
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/inquery", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ category, email, message }),
+      });
 
       if (response.ok) {
         setSubmitSuccess(true);
