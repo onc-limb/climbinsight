affbbe3d1f75a4592ecef70b9ac3e83a2cbd91e5
onsato <nogiglee@gmail.com>
2025-08-13 12:07:28 +0900

    add supabase

---
+++ frontend/src/lib/supabaseClient.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const signUp = async (input: { email: string; password: string }) => {
+  return await supabase.auth.signUp({
+    email: input.email,
+    password: input.password,
+  });
+};
